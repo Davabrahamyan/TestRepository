bdd6e06ea43a26137be196c71a48156d865872d0
Samvel Hovhannisyan <sam.h.sam2005@gmail.com>
2023-02-05 17:57:53 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+class Page{
+    String url;
+    Integer viewCount;
+}
+public class Main {
+    MyLinkedList<String> linkedList = new MyLinkedList<>();
+    public static void main(String[] args){
+
+        Stack<Page> st = new Stack<>();
+    }
+
+
+}
